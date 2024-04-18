70850a18757a99d4c7244ec03c5956dc4d60c9ca
Appmaniazar-Projects <appmaniazar@gmail.com>
2024-04-18 10:33:10 +0200

    Rendered the navigation component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import React from 'react';
 // import PosterEditor from './MyEditor';
 import TaskListPro from './components/TaskListPro';
 import MyEditor from './components/MyEditor';
+import NavBar from './components/NavBar/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <MyEditor />
+      {/* <MyEditor /> */}
+      <NavBar />
     </div>
   );
 }
