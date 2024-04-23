a4570f297beba57f5c82cf9060b9494d0429d39a
Appmaniazar-Projects <appmaniazar@gmail.com>
2024-04-23 14:25:12 +0200

    Changed the rendered components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 // App.js
 import React from 'react';
 // import PosterEditor from './MyEditor';
-import TaskListPro from './components/TaskListPro';
+// import TaskListPro from './components/TaskListPro';
 import MyEditor from './components/MyEditor';
 import NavBar from './components/NavBar/NavBar';
 
 function App() {
   return (
     <div className="App">
-      {/* <MyEditor /> */}
       <NavBar />
+      {/* <MyEditor /> */}
     </div>
   );
 }
